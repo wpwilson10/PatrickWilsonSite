e4b97e62c65856a0d4dd0fdc78199cc552e1fafb
wpwilson10 <wpwilson10@gmail.com>
2023-01-14 15:02:27 -0600

    Contact Links with Icons
    Contact section now includes links to GitHub, LinkedIn, and email with an icon for each link.

--- client/src/components/ContactInfo/contactInfo.tsx
+++ client/src/components/ContactInfo/contactInfo.tsx
@@ -1,5 +1,7 @@
-import { Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import ContactFormRecaptcha from "../ContactForm/contactFormRecaptcha";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 
 // --- React component
 const ContactInfoInner = () => {
@@ -9,8 +11,54 @@ const ContactInfoInner = () => {
 				Inner container created padding around form  */}
 			<Container className="p-4">
 				<h3>Contact Information</h3>
+				<p></p>
 				<p>Email: person@gmail.com</p>
-				<p>Github: acccount@github.com</p>
+				<Row className="justify-content-start">
+					<Col xs={12} md="auto">
+						{/* GitHub Icon and Link 
+							Target and Rel options cause the link to open in a new tab */}
+						<a
+							href="http://github.com/wpwilson10"
+							target="_blank"
+							rel="noopener noreferrer"
+							className="link-no-decoration"
+						>
+							<div className="contact-link-container">
+								<FaGithub size="2em" />
+								<div className="contact-link-text">GitHub</div>
+							</div>
+						</a>
+					</Col>
+					<Col xs={12} md="auto">
+						{/* LinkedIn Icon and Link
+							Target and Rel options cause the link to open in a new tab */}
+						<a
+							href="https://linkedin.com"
+							target="_blank"
+							rel="noopener noreferrer"
+							className="link-no-decoration"
+						>
+							<div className="contact-link-container">
+								<FaLinkedin size="2em" />
+								<div className="contact-link-text">
+									LinkedIn
+								</div>
+							</div>
+						</a>
+					</Col>
+					<Col xs={12} md="auto">
+						{/* Opens email */}
+						<a
+							href="mailto:wpwilson10@gmail.com"
+							className="link-no-decoration"
+						>
+							<div className="contact-link-container">
+								<MdEmail size="2em" />
+								<div className="contact-link-text">Email</div>
+							</div>
+						</a>
+					</Col>
+				</Row>
 			</Container>
 		</Container>
 	);
